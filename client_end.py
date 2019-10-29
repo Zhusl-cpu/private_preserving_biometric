394a1aad926b0e0d4371c0d1565f012691056899
Zhusl-cpu <56342960+Zhusl-cpu@users.noreply.github.com>
2019-10-29 11:23:37 -0700

    Add files via upload

--- client_end.py
+++ client_end.py
@@ -1,14 +1,14 @@
 import base64
 import eel
-from socket import *
+import socket
 from api import load_image_file
 from api import face_encodings
 
-eel.init('C:\\Users\\86139\\PycharmProjects\\untitled')
+eel.init('')
 
 
 @eel.expose
-def img_to_vector(base64_data):
+def sendVector(base64_data, id, status):
     base64_data = base64_data.split(',')[1]
     base64_data = str.encode(base64_data)
     data = base64.b64decode(base64_data)
@@ -18,11 +18,19 @@ def img_to_vector(base64_data):
     photo = load_image_file('prtsc.png')
     face_vectors = face_encodings(photo)
     face = face_vectors[0]
+    face = 'data:' + str(face)
+    function = 'function:' + str(status)
+    userid = 'id:' + str(id)
     print(face)
-    # addr = ('localhost', 21567)
-    # sock = socket(AF_INET, SOCK_DGRAM)
-    # sock.sendto(face, //addr)
-    # sock.close()
+    addr = ('122.51.26.166', 22222)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(addr)
+    msg = userid + '\r\n' + function + '\r\n' + face
+    msg = str.encode(msg)
+    sock.send(msg)
+    feedback = sock.recv(1024)
+    print(feedback)
+    sock.close()
 
 
-eel.start('home.html', size=(1000, 1000))
+eel.start('home.html', mode='firefox', size=(1000, 1000))
